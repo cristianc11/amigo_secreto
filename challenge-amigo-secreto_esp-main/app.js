ec2a6b58f30a6792ec3ab002cf8bd7cbf3f1cbca
cristianc11 <cmirjs03@gmail.com>
2025-01-28 22:07:09 -0500

    create logic app.js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if (nombre){
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        actualizarAmigos();
+    }
+    else{
+        alert("Por favor, inserte un nombre");
+    }
+}
+
+function actualizarAmigos(){
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement("li");
+        document.getElementById("listaAmigos").appendChild(item);
+        item.innerHTML = amigos[i];
+    }
+}
+
+
+function sortearAmigo(){
+    if(amigos.length > 0){
+        let indice = Math.floor(Math.random()*amigos.length);
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = ("Tu amigo secreto es "+amigos[indice]);
+        document.getElementById("listaAmigos").innerHTML = "";
+    }
+    else{
+        alert("Por favor, inserte al menos un nombre");
+    }
+}
+
+
